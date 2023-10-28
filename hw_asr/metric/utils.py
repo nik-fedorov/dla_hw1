55184df72a46b9173cb5eaa0746b960902f60d59
nik-fedorov <nik-fedorov-work@yandex.ru>
2023-10-28 14:37:32 +0300

    fix bug with confused wer and cer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -2,12 +2,12 @@ import editdistance
 
 
 def calc_cer(target_text, predicted_text) -> float:
+    denominator = len(target_text) or 1
+    return editdistance.eval(target_text, predicted_text) / denominator
+
+
+def calc_wer(target_text, predicted_text) -> float:
     target_text_splitted = target_text.split()
     predicted_text_splitted = predicted_text.split()
     denominator = len(target_text_splitted) or 1
     return editdistance.eval(target_text_splitted, predicted_text_splitted) / denominator
-
-
-def calc_wer(target_text, predicted_text) -> float:
-    denominator = len(target_text) or 1
-    return editdistance.eval(target_text, predicted_text) / denominator
